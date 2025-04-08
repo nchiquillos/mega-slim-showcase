ec360f9e5bc6df0ee7a4b16a7ef5829d20ae0948
nchiquillos <141120640+nchiquillos@users.noreply.github.com>
2025-04-08 19:41:13 +0000

    cambios

--- src/assets/productImages.ts
+++ src/assets/productImages.ts
@@ -5,8 +5,8 @@ import defaultProductImage from '@/assets/Natural-PostCuadrado.png';
 // External image URLs
 const aquaPlusImage = 'https://acasas.2com.co/AguaPlusMockupFondo.png';
 const digestPlusImage = 'https://acasas.2com.co/DigestPlusMockupFondo.png';
-const glucoseLifePlusImage = 'https://acasas.2com.co/MockupNatural%20(1).png';
-const megaSlimPlusImage = 'https://acasas.2com.co/MockupNatural-1%20(1).png';
+const glucoseLifePlusImage = 'https://acasas.2com.co/MockupNatural-1%20(1).png';
+const megaSlimPlusImage = 'https://acasas.2com.co/MockupNatural%20(1).png';
 const heroMainImage = 'https://acasas.2com.co/Disen%CC%83o%20sin%20ti%CC%81tulo.png';
 
 export const productImages = {
